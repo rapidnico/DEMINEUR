aab3a17fce721b903e572f278df6c6b84ebeaa11
nicor <nicor@LAPTOP-1DUDVVLI>
2020-12-09 20:44:33 +0100

    ajout comm et changement variables

--- src/main/java/Vue.java
+++ src/main/java/Vue.java
@@ -25,11 +25,11 @@ public class Vue extends JFrame {
     public void selection(int x, int y) {
 
         cases[x][y].show();
-        if (cases[x][y].getnumber() == 0) {
-            for (int i = 0; i < 3; i++) {
+        if (cases[x][y].getnumber() == 0) {//si il n'y a pas de bombe autour
+            for (int i = 0; i < 3; i++) {//on parcours les cases autour de la clase cliquée
                 for (int j = 0; j < 3; j++) {
                     if (x + i > 0 && x - 1 + i < this.largeur && y + j > 0 && y - 1 + j < this.haut) {
-
+//on vérifie qu'on reste dans la grille
                         if (!cases[x - 1 + i][y - 1 + j].shown()) {
                             this.selection(x - 1 + i, y - 1 + j);
                         }
@@ -37,18 +37,20 @@ public class Vue extends JFrame {
                 }
             }
         }
-        if (cases[x][y].isbomb()) {
 
-            defaite();
-            this.refresh();
+        if (cases[x][y].isbomb()) {//si la case cliquée est une bombe
+
+            defaite();//on perd
+            this.refresh();//on actualise l'interface
+
         }
-        int com = 0;
+        int com = 0;//compteur de coup
 
-        for (int i = 0; i < this.largeur; i++) {
-            for (int j = 0; j < this.haut; j++) {
-                if (cases[i][j].isbomb() || cases[i][j].shown()) {
-                    com++;//compteur de coup
-                }
+        for (int i = 0; i < this.largeur; i++) {//on parcours toute la grille
+            for (int j = 0; j < this.haut; j++) {//le comtpeur de coup comptera le nombre de bombe+le nombre de case dévoilées
+                if (cases[i][j].isbomb() || cases[i][j].shown()) {//comme ça, quand le compteur de coup atteint la taille de
+                    com++;//la grille, le jeu sait que toutes les cases possibles a jouer sans perdre
+                }//ont été jouées.
             }
         }
         if (com == this.largeur * this.haut) {
@@ -72,9 +74,7 @@ public class Vue extends JFrame {
             }
         }
     }
-
-    
-
+ 
     public void NouvelleGrille() {//creation de la grille
         grille = new Grille(this); //Cree une nouvelle grille
         reset.setSize(grille.getWidth(), 100);
@@ -90,8 +90,6 @@ public class Vue extends JFrame {
         setVisible(true);
     }
 
-    
-
     public Vue(int d, int lon, int lar) {//creation de la fenetre de jeu
         victoire = false;
         this.haut = lon;
@@ -106,7 +104,7 @@ public class Vue extends JFrame {
         reset.setText("Recommencer");
         add(grille, BorderLayout.CENTER); //Place la grille
 
-        add(reset, BorderLayout.SOUTH); //Place le bouton recommencer en bas
+        add(reset, BorderLayout.SOUTH); //ajout de bouton recommencer en bas
 
         setTitle("Démineur");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -114,7 +112,7 @@ public class Vue extends JFrame {
         pack();
         setVisible(true);
     }
-    
+
     public int getDifficulte() { //les 3 methodes pour recuperer la taille de la grille et la diffictulté(nb bombes)
         return this.difficulte;
     }
@@ -123,14 +121,14 @@ public class Vue extends JFrame {
         return this.haut;
     }
 
-        public int getLargeur() {
+    public int getLargeur() {
         return this.largeur;
     }
 
     public JButton getreset() {
         return this.reset;
     }
-    
+
     public boolean victoire() {
         return victoire;
     }
